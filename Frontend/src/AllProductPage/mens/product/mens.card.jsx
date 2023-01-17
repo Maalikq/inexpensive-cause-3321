c901f3bb2e093ff330f85b4b690ad8da0eb101b6
deepak65190 <deepakbisht@gmail.com>
2023-01-17 11:34:21 +0530

    mens page

---
+++ Frontend/src/AllProductPage/mens/product/mens.card.jsx
@@ -0,0 +1,12 @@
+import styles from "./mens.card.module.css";
+
+
+const MensCard = () => {
+
+  return (
+    <div className={styles.container}>
+    <h1>hellow</h1>
+    </div>
+  );
+};
+export default MensCard;
