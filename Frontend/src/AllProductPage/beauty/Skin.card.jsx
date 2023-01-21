52cf0920c912a6c600b11d36a0056e96f77efb6c
deepak65190 <deepakbisht@gmail.com>
2023-01-21 17:38:05 +0530

    all product page completed

--- Frontend/src/AllProductPage/beauty/Skin.card.jsx
+++ Frontend/src/AllProductPage/beauty/Skin.card.jsx
@@ -1,8 +1,22 @@
 import styles from "./Skin.card.module.css";
-
+import { FaStar, FaStarHalfAlt } from "react-icons/fa";
+import { AiOutlineStar } from "react-icons/ai";
 const SkinCard = ({ image, name, price, offPrice, rating }) => {
   let offPercentage = Math.ceil(100 - (offPrice / price) * 100);
-
+let startRating = Array.from({ length: 5 }, (ele, index) => {
+  let number = index + 0.5;
+  return (
+    <>
+      {rating >= index + 1 ? (
+        <FaStar color="#FFC315" />
+      ) : rating >= number ? (
+        <FaStarHalfAlt color="#FFC315" />
+      ) : (
+        <AiOutlineStar color="#FFC315" />
+      )}
+    </>
+  );
+});
   return (
     <div className={styles.Cardcontainer}>
       <div className={styles.pImage}>
@@ -14,7 +28,7 @@ const SkinCard = ({ image, name, price, offPrice, rating }) => {
         <span className={styles.offPricemens}>Rs. {offPrice} </span>
         <button className={styles.offbtn}>{offPercentage}% off</button>
       </p>
-      <div>{rating}</div>
+      <div className={styles.ratingflex}>{startRating}</div>
     </div>
   );
 };
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -9,14 +9,14 @@ import MensProducts from './AllProductPage/mens/product/shoes';
 import Tshirt from './AllProductPage/mens/product/tShirt';
 import Sarees from './AllProductPage/womens/Sarees';
 import Skin from './AllProductPage/beauty/Skin';
-//import Cart from './components/Cart';
+import Cart from './components/Cart';
 import Allroute from './components/Allroute';
 function App() {
   return (
     <div className="App">
-      {/* <Allroute/>
-       <Cart/>  */}
-       <Skin/>
+      <Allroute/>
+       <Cart/> 
+       {/* <Skin/> */}
   {/* <MensProducts/>   */}
     {/* <Sarees/> */}
   {/* <Tshirt/>   */}
